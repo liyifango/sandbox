fe74646d05cd5e0f67b99d42e4ea9fd70fa36aab
李一凡 <liyifango@gmail.com>
2020-08-28 10:48:49 +0800

    feat: 测试nacos注册中心集成

--- src/main/java/com/sinosoft/hanlin/sandbox/SandboxApplication.java
+++ src/main/java/com/sinosoft/hanlin/sandbox/SandboxApplication.java
@@ -3,11 +3,13 @@ package com.sinosoft.hanlin.sandbox;
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @EnableKnife4j
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SandboxApplication {
 
 	public static void main(String[] args) {
